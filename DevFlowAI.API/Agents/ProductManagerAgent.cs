f1e98c2aac76dab5e140dd7489e6539115812d7e
Sanan Malik <iamsanan50@gmail.com>
2026-06-12 21:32:43 +0500

    little changes creating database and linking

---
+++ DevFlowAI.API/Agents/ProductManagerAgent.cs
@@ -0,0 +1,14 @@
+﻿using DevFlowAI.API.Interfaces;
+
+namespace DevFlowAI.API.Agents
+{
+    public class ProductManagerAgent : IAgent
+    {
+        public async Task<string> ExecuteAsync(string context)
+        {
+            await Task.Delay(100);
+
+            return $"Requirements generated for: {context}";
+        }
+    }
+}
--- DevFlowAI.API/Program.cs
+++ DevFlowAI.API/Program.cs
@@ -1,18 +1,20 @@
+using DevFlowAI.API.Data;
+using DevFlowAI.API.Agents;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<ProductManagerAgent>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
